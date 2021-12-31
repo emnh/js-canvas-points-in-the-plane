0f17166115abe2d333c446520ff090e6d5801571
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2021-12-31 15:25:55 +0100

    upd

--- src/index.js
+++ src/index.js
@@ -2,8 +2,145 @@ const $ = require('jquery');
 const dat = require('dat.gui');
 import GPU from 'gpu.js';
 import SimplexNoise from 'simplex-noise';
-
 const seedrandom = require('seedrandom');
+
+
+const addNoiseFunctions = function(GPU) {
+	//
+	// Description : Array and textureless GLSL 2D/3D/4D simplex
+	//               noise functions.
+	//      Author : Ian McEwan, Ashima Arts.
+	//  Maintainer : ijm
+	//     Lastmod : 20110822 (ijm)
+	//     License : Copyright (C) 2011 Ashima Arts. All rights reserved.
+	//               Distributed under the MIT License. See LICENSE file.
+	//               https://github.com/ashima/webgl-noise
+	//
+
+//  GPU.addNativeFunction('mod2893',
+//  `
+//    vec3 mod2893(vec3 x) {
+//      return x - floor(x * (1.0 / 289.0)) * 289.0;
+//    }
+//  `
+//  );
+
+//  GPU.addNativeFunction('mod2894',
+//  `
+//  vec4 mod2894(vec4 x) {
+//    return x - floor(x * (1.0 / 289.0)) * 289.0;
+//  }
+//  `);
+
+//  GPU.addNativeFunction('permute',
+//  `
+//  vec4 permute(vec4 x) {
+//       return mod2894(((x*34.0)+1.0)*x);
+//  }
+//  `);
+
+//  GPU.addNativeFunction('taylorInvSqrt',
+//  `
+//  vec4 taylorInvSqrt(vec4 r)
+//  {
+//    return 1.79284291400159 - 0.85373472095314 * r;
+//  }
+//  `);
+
+	GPU.addNativeFunction('snoise',
+	`
+	float snoise4(vec3 v) {
+    return 0.0;
+  }
+	vec3 mod289(vec3 x) {
+		return x - floor(x * (1.0 / 289.0)) * 289.0;
+	}
+	vec4 mod289(vec4 x) {
+		return x - floor(x * (1.0 / 289.0)) * 289.0;
+	}
+	vec4 permute(vec4 x) {
+			 return mod289(((x*34.0)+1.0)*x);
+	}
+	vec4 taylorInvSqrt(vec4 r)
+	{
+		return 1.79284291400159 - 0.85373472095314 * r;
+	}
+	float snoise(vec3 v)
+		{
+		const vec2  C = vec2(1.0/6.0, 1.0/3.0) ;
+		const vec4  D = vec4(0.0, 0.5, 1.0, 2.0);
+
+	// First corner
+		vec3 i  = floor(v + dot(v, C.yyy) );
+		vec3 x0 =   v - i + dot(i, C.xxx) ;
+
+	// Other corners
+		vec3 g = step(x0.yzx, x0.xyz);
+		vec3 l = 1.0 - g;
+		vec3 i1 = min( g.xyz, l.zxy );
+		vec3 i2 = max( g.xyz, l.zxy );
+
+		//   x0 = x0 - 0.0 + 0.0 * C.xxx;
+		//   x1 = x0 - i1  + 1.0 * C.xxx;
+		//   x2 = x0 - i2  + 2.0 * C.xxx;
+		//   x3 = x0 - 1.0 + 3.0 * C.xxx;
+		vec3 x1 = x0 - i1 + C.xxx;
+		vec3 x2 = x0 - i2 + C.yyy; // 2.0*C.x = 1/3 = C.y
+		vec3 x3 = x0 - D.yyy;      // -1.0+3.0*C.x = -0.5 = -D.y
+
+	// Permutations
+		i = mod289(i);
+		vec4 p = permute( permute( permute(
+							 i.z + vec4(0.0, i1.z, i2.z, 1.0 ))
+						 + i.y + vec4(0.0, i1.y, i2.y, 1.0 ))
+						 + i.x + vec4(0.0, i1.x, i2.x, 1.0 ));
+
+	// Gradients: 7x7 points over a square, mapped onto an octahedron.
+	// The ring size 17*17 = 289 is close to a multiple of 49 (49*6 = 294)
+		float n_ = 0.142857142857; // 1.0/7.0
+		vec3  ns = n_ * D.wyz - D.xzx;
+
+		vec4 j = p - 49.0 * floor(p * ns.z * ns.z);  //  mod(p,7*7)
+
+		vec4 x_ = floor(j * ns.z);
+		vec4 y_ = floor(j - 7.0 * x_ );    // mod(j,N)
+
+		vec4 x = x_ *ns.x + ns.yyyy;
+		vec4 y = y_ *ns.x + ns.yyyy;
+		vec4 h = 1.0 - abs(x) - abs(y);
+
+		vec4 b0 = vec4( x.xy, y.xy );
+		vec4 b1 = vec4( x.zw, y.zw );
+
+		//vec4 s0 = vec4(lessThan(b0,0.0))*2.0 - 1.0;
+		//vec4 s1 = vec4(lessThan(b1,0.0))*2.0 - 1.0;
+		vec4 s0 = floor(b0)*2.0 + 1.0;
+		vec4 s1 = floor(b1)*2.0 + 1.0;
+		vec4 sh = -step(h, vec4(0.0));
+
+		vec4 a0 = b0.xzyw + s0.xzyw*sh.xxyy ;
+		vec4 a1 = b1.xzyw + s1.xzyw*sh.zzww ;
+
+		vec3 p0 = vec3(a0.xy,h.x);
+		vec3 p1 = vec3(a0.zw,h.y);
+		vec3 p2 = vec3(a1.xy,h.z);
+		vec3 p3 = vec3(a1.zw,h.w);
+
+	//Normalise gradients
+		vec4 norm = taylorInvSqrt(vec4(dot(p0,p0), dot(p1,p1), dot(p2, p2), dot(p3,p3)));
+		p0 *= norm.x;
+		p1 *= norm.y;
+		p2 *= norm.z;
+		p3 *= norm.w;
+
+	// Mix final noise value
+		vec4 m = max(0.6 - vec4(dot(x0,x0), dot(x1,x1), dot(x2,x2), dot(x3,x3)), 0.0);
+		m = m * m;
+		return 42.0 * dot( m*m, vec4( dot(p0,x0), dot(p1,x1),
+																	dot(p2,x2), dot(p3,x3) ) );
+	}
+	`);
+};
 
 const createShape = function (simplex, ws, hs) {
   const colors = new Float32Array(4 * ws * hs);
@@ -15,7 +152,6 @@ const createShape = function (simplex, ws, hs) {
       const dx = x - 0.5 * ws;
       const dy = y - 0.5 * hs;
       const radius = 0.5 * Math.max(ws, hs);
-      const innerCircle = Math.sqrt(dx * dx + dy * dy) <= radius;
       const vradius =
         (0.75 + 0.25 * Math.sin(10.0 * Math.atan2(dy, dx))) * radius;
       const lerp = (x, y, a) => x * (1 - a) + y * a;
@@ -116,10 +252,11 @@ const main = function(options) {
     pointsColorB[i] = point.color.b;
   }
 
-	const ws = 100;
-	const hs = 100;
-  const shapeCount = 100;	
+	const ws = options.shapeWidth;
+	const hs = options.shapeHeight;
+  const shapeCount = 100;
   const shapeLocations = new Float32Array(shapeCount * 2);
+  const shapeColors = new Float32Array(shapeCount * 3);
 	const shapes = new Float32Array(shapeCount * 4 * ws * hs);
   for (let i = 0; i < shapeCount; i++) {
     const shape = createShape(simplex, ws, hs);
@@ -130,6 +267,9 @@ const main = function(options) {
     };
     shapeLocations[i * 2] = Math.floor(Math.random() * w);
     shapeLocations[i * 2 + 1] = Math.floor(Math.random() * h);
+    shapeColors[i * 3 + 0] = color.r;
+    shapeColors[i * 3 + 1] = color.g;
+    shapeColors[i * 3 + 2] = color.b;
     for (let x = 0; x < ws; x++) {
       for (let y = 0; y < hs; y++) {
         const idx = 4 * (y * ws + x);
@@ -144,10 +284,41 @@ const main = function(options) {
 
 
   const gpu = new GPU.GPU();
+	addNoiseFunctions(gpu);
+  function lerp2(x, y, a) {
+    return x * (1.0 - a) + y * a;
+  }
+  function snoise3(a, b, c) {
+    const ret = snoise([a, b, c]);
+    return ret;
+  }
+  function createShapeGPU(initRadius, spikiness, shapeSeed, ws, hs, x, y) {
+    const dx = x - 0.5 * ws;
+    const dy = y - 0.5 * hs;
+    const radius = 0.5 * Math.max(ws, hs);
+    const vradius =
+      (0.75 + 0.25 * Math.sin(10.0 * Math.atan2(dy, dx))) * radius;
+    const angle = Math.atan2(dy, dx) + Math.PI;
+    const nangle = angle / Math.PI / 2.0;
+    const c = 1.0;
+    const from = (initradius + spikiness * Math.abs(snoise3(c * angle, shapeSeed, 0.0))) * radius;
+    const to = (initradius + spikiness * Math.abs(snoise3(c * -angle, shapeSeed, 0.0))) * radius;
+    const vradius2 = lerp2(from, to, nangle);
+    const currentRadius = Math.sqrt(dx * dx + dy * dy);
+    const border = currentRadius >= vradius2 && currentRadius <= vradius2 + 1.0;
+    const bc1 = snoise3((0.5 * x) / ws, (0.5 * y) / ws, shapeSeed);
+    const bc2 = 0.5 * bc1 + 1.0;
+    const shape = currentRadius <= vradius2 ? bc2 : 0.0;
+    return shape;
+  };
+	gpu.setFunctions([lerp2, snoise3, createShapeGPU]);
+
   const maxd = Math.sqrt(w * w + h * h);
   const maxe = 1.0 / pointCount;
   const process = gpu
-    .createKernel(function (pointsX, pointsY, pointsColorR, pointsColorG, pointsColorB, shapes, shapeLocations) {
+    .createKernel(function (
+      pointsX, pointsY, pointsColorR, pointsColorG, pointsColorB,
+      shapes, shapeLocations, shapeColors) {
       let r = 0.0;
       let g = 0.0;
       let b = 0.0;
@@ -176,12 +347,22 @@ const main = function(options) {
         const posX = shapeLocations[i * 2];
         const posY = shapeLocations[i * 2 + 1];
         if (x >= posX && x < posX + ws && y >= posY && y < posY + hs) {
-          const idx = 4 * ((y - posY) * ws + (x - posX));
+          const lx = x - posX;
+          const ly = y - posY;
+          const idx = 4 * (ly * ws + lx);
           const gidx = 4 * i * ws * hs + idx;
-          r += shapes[gidx + 0];
-          g += shapes[gidx + 1];
-          b += shapes[gidx + 2];
-          a += shapes[gidx + 3];
+//          r += shapes[gidx + 0];
+//          g += shapes[gidx + 1];
+//          b += shapes[gidx + 2];
+//          a += shapes[gidx + 3];
+          const shapeSeed = i;
+          const ar =
+            createShapeGPU(
+              this.constants.initRadius, this.constants.spikiness, shapeSeed, ws, hs, lx, ly);
+          r += ar * shapeColors[i * 3 + 0];
+          g += ar * shapeColors[i * 3 + 1];
+          b += ar * shapeColors[i * 3 + 2];
+          a += ar;
         }
       }
       return [r, g, b, 1];
@@ -195,12 +376,17 @@ const main = function(options) {
         lightDecay: options.lightDecay,
         shapeCount,
         shapeWidth: ws,
-        shapeHeight: hs
+        shapeHeight: hs,
+        initRadius: options.initRadius,
+        spikiness: options.spikiness
       },
       output: [w, h]
     });
 
-  const out = process(pointsX, pointsY, pointsColorR, pointsColorG, pointsColorB, shapes, shapeLocations);
+  const out =
+    process(
+      pointsX, pointsY, pointsColorR, pointsColorG, pointsColorB,
+      shapes, shapeLocations, shapeColors);
 
   // Get normalize factor
   let [maxr, maxg, maxb] = [0.0, 0.0, 0.0];
@@ -246,7 +432,12 @@ const datgui = function() {
     pointCount: 100,
     pointsRandomColor: true,
     pointsColor: [0, 0, 0, 0],
-    lightDecay: 1.0
+    lightDecay: 1.0,
+    shapesEnabled: true,
+    shapeWidth: 100,
+    shapeHeight: 100,
+    initRadius: 0.5,
+    spikiness: 0.5
   };
   const reset = function() {
     main(options);
@@ -263,6 +454,12 @@ const datgui = function() {
   points.addColor(options, 'pointsColor').onFinishChange(reset);
   const lights = gui.addFolder('Light Options');
   lights.add(options, 'lightDecay', 0.0, 10.0).onFinishChange(reset);
+  const shapes = gui.addFolder('Shape Options');
+  shapes.add(options, 'shapesEnabled').onFinishChange(reset);
+  shapes.add(options, 'shapeWidth', 0.0, 500.0).onFinishChange(reset);
+  shapes.add(options, 'shapeHeight', 0.0, 500.0).onFinishChange(reset);
+  shapes.add(options, 'initRadius', 0.0, 5.0).onFinishChange(reset);
+  shapes.add(options, 'spikiness', 0.0, 5.0).onFinishChange(reset);
   reset();
 };
 
